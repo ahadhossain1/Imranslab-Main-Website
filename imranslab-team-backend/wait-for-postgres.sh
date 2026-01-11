b52779d28829663bdf8e645994752979945810e5
Md Ahad Hossain <117075956+ahadhossain1@users.noreply.github.com>
2026-01-11 12:33:44 +0600

    Delete imranslab-team-backend directory

--- imranslab-team-backend/wait-for-postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-until PGPASSWORD=$DATABASE_PASSWORD psql -h postgres-team-db -U postgres -c '\l'; do
-  echo "Waiting for PostgreSQL to be ready..."
-  sleep 2
-done
-
-# Start the Django app
-python manage.py migrate
-exec "$@"
