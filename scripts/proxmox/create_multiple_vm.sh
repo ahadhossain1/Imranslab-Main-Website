c4a5a15d57d8b6489384855a2b8f7f5aa75242df
Md Ahad Hossain <117075956+ahadhossain1@users.noreply.github.com>
2026-01-11 12:34:52 +0600

    Delete scripts directory

--- scripts/proxmox/create_multiple_vm.sh
+++
@@ -1,7 +0,0 @@
-for i in {101..110}; do
-  qm create $i --name vm-$i --memory 2048 --net0 virtio,bridge=vmbr0 --bootdisk scsi0 --scsihw virtio-scsi-pci
-  qm set $i --scsi0 local-lvm:10
-  qm set $i --cdrom local:iso/ubuntu-22.04.iso
-  qm set $i --boot order=scsi0;ide2
-  qm start $i
-done
--- scripts/proxmox/install_qemu_guest_agent.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-sudo apt install -y qemu-guest-agent
-sudo systemctl enable qemu-guest-agent
-sudo systemctl start qemu-guest-agent
-sudo systemctl status qemu-guest-agent
